9f710ea8a0aa59a8ea702b2b011978938ee9dd35
Kelson-Carvalho <kelsonfabriciocarvalho@gmail.com>
2024-02-19 00:53:58 -0300

    removendo estrutura manual para função pegar dados da api

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,14 @@
 import DayState from "@/components/DayState";
+import { kv } from "@vercel/kv";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
-  const desafios = {
-    "Estudar javascript": {
-      "2024-02-16": false,
-      "2024-02-15": true,
-      "2024-02-14": true,
-    },
-    "Estudar Flutter": {
-      "2024-02-13": true,
-      "2024-02-12": true,
-      "2024-02-11": false,
-    },
-  };
+type Desafio = {
+  [desfio: string]: Record<string, boolean>;
+} | null;
+
+export default async function Home() {
+  const desafios: Desafio = await kv.hgetall("desafios");
 
   const today = new Date();
   const todayWeekDay = today.getDay();
@@ -24,6 +18,17 @@ export default function Home() {
   const sorteWeekDays = weekDays
     .slice(todayWeekDay + 1)
     .concat(weekDays.slice(0, todayWeekDay + 1));
+
+  const last7Days = weekDays
+    .map((_, index) => {
+      const date = new Date();
+      date.setDate(date.getDate() - index);
+
+      return date.toISOString().slice(0, 10);
+    })
+    .reverse();
+
+  console.log(last7Days);
 
   return (
     <main className="container relative flex flex-col gap-8 px-4 pt-16">
@@ -47,25 +52,29 @@ export default function Home() {
                 />
               </button>
             </div>
-            <section className="grid grid-cols-7 bg-neutral-800 rounded-md p-2">
-              {sorteWeekDays.map((dia) => (
-                <div key={dia} className="flex flex-col last:font-bold">
-                  <span className="font-sans text-xs text-white text-center">
-                    {dia}
-                  </span>
-                  <DayState dia={undefined} />
-                </div>
-              ))}
-            </section>
+            <Link href={`desafio/${desafio}`}>
+              <section className="grid grid-cols-7 bg-neutral-800 rounded-md p-2">
+                {sorteWeekDays.map((dia, index) => (
+                  <div key={dia} className="flex flex-col last:font-bold">
+                    <span className="font-sans text-xs text-white text-center">
+                      {dia}
+                    </span>
+                    <DayState dia={desafiosStreak[last7Days[index]]} />
+                  </div>
+                ))}
+              </section>
+            </Link>
           </div>
         ))}
 
-        <Link 
+      <Link
         href="/novo-desafio"
         className="fixed text-center bottom-10 w-2/3
         left-1/2 -translate-x-1/2 text-neutral-900 bg-[#05EDFB] font-display font-bold text-3xl p-2
         rounded-md"
-        >Novo desafio</Link>
+      >
+        Novo desafio
+      </Link>
     </main>
   );
 }
